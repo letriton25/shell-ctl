38d3e36be661d53f12cb66856afdedd2997a74e4
Tristan AMIOTTE-SUCHET <tamiottesuchet@gmail.com>
2025-10-22 17:23:19 +0300

    feat: entry point function for CLI

---
+++ shctl.sh
@@ -0,0 +1,55 @@
+################
+# CLI FUNCTION #
+################
+
+shctl ()
+{
+    # variables
+    local shctl_dir=~/.bashrc.d
+    local src_dir=${shctl_dir}/src
+    local script_dir=${shctl_dir}/scripts
+
+    local usage_path="${src_dir}/shctl-usage.sh"
+    local version_path="${src_dir}/shctl-version.sh"
+
+    local config_path="${shctl_dir}/shctl.conf"
+
+    # check arguments
+    if [ $# -eq 0 ]; then
+        . "${usage_path}"
+        return 1
+    fi
+    local cmd="$1"
+    local cmd_path="${src_dir}/shctl-${cmd}.sh"
+    shift
+
+    # command parsing
+    case "$cmd" in
+        start|stop|enable|disable|status)
+            if [ $# -ne 1 ]; then
+                echo >&2 "Error: Script name required for '$cmd' command."
+                . "${usage_path}"
+                return 1
+            fi
+            local script_name="$1"
+            local script_path="${script_dir}/${script_name}.sh"
+            if [ ! -f "${script_path}" ]; then
+                echo >&2 "Error: Script '${script_name}' not found."
+                return 1
+            fi
+            . "${cmd_path}" "${script_path}" "${config_path}"
+            ;;
+        help|--help)
+            . "${usage_path}"
+            ;;
+        --version)
+            . "${version_path}"
+            ;;
+        *)
+            echo >&2 "Error: Unknown command '$cmd'."
+            . "${usage_path}"
+            return 1
+            ;;
+    esac
+    return 0
+}
---
+++ src/shctl-version.sh
@@ -0,0 +1,1 @@
+echo "shctl version 1.0.0"
